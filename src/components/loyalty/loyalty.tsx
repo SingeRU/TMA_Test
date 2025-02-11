5d521f064d17745d0f1a42621d09da911c70518b
Lesha Pletukhin <mrster03@mail.ru>
2025-02-11 16:12:39 +0300

    feat!: main screen UI extended

---
+++ src/components/loyalty/loyalty.tsx
@@ -0,0 +1,8 @@
+export const Loyalty = () => {
+	return (
+		<div className="text-center space-y-4">
+			<h1 className="text-2xl font-bold">Количество баллов</h1>
+			<p className="text-4xl">0</p>
+		</div>
+	);
+};
--- src/components/shop/qrCode/qrCode.tsx
+++ src/components/shop/qrCode/qrCode.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export const QrCode = () => {
-	return <Image src={"/qrCode.svg"} alt="QrCode" width={200} height={200} />;
+	return (
+		<div>
+			<Image src={"/qrCode.svg"} alt="QrCode" width={200} height={200} />
+		</div>
+	);
 };
